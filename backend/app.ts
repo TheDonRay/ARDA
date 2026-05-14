4ffc7259643ed1f68a1bae8b3bb3fb0713f399db
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-05-13 20:44:37 -0400

    working on the backend

--- backend/app.ts
+++ backend/app.ts
@@ -2,3 +2,11 @@ import express, {Request, Response} from 'express';
 const app = express(); 
 
 app.use(express.json()); // some built in middleware handling that I put 
+
+app.get('/ARDA', (req: Request, res: Response) => { 
+    res.json({ 
+        Server: 'Welcome to ARDA, simple backend API to understand how data is transferred through JWT and session managed'
+    }); 
+}); 
+
+export default app; 
