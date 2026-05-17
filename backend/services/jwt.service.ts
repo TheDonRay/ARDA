5851501e60cbc1615b23a54029c69fe865a4a95a
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-05-16 21:25:43 -0400

    working on services

--- backend/services/jwt.service.ts
+++ backend/services/jwt.service.ts
@@ -1,8 +1,17 @@
-import jwt from 'jsonwebtoken'; 
+import * as jwt from 'jsonwebtoken'; 
 
 // import our secret keys here 
 const SECRET = process.env.JWT_SECRET as string; 
-const EXPIRATION = process.env.JWT_EXPIRES_IN as string; 
+const EXPIRATION = process.env.JWT_EXPIRES_IN as string;  
+
+// add checks for the env 
+if (!SECRET) { 
+    throw new Error ('Error no secret provided'); 
+} 
+
+if (!EXPIRATION) { 
+    throw new Error ('Error no expiration time provided'); 
+}; 
 
 // set the default payload here 
 export interface jwtPayload { 
@@ -13,6 +22,8 @@ export interface jwtPayload {
 
 // a function to actually create a token from the payload 
 export function signToken(payload: jwtPayload) : string { 
-    const token = jwt.sign(payload, SECRET, { expiresIn: EXPIRATION});  
+    const token = jwt.sign(payload, SECRET, { expiresIn: EXPIRATION } as jwt.SignOptions);
     return token; 
-}
+}; 
+
+/*So when it comes to verifying token function what happens is that we take the incoming request which has to be the same type of data that we are expecting in our payload, check the "stamp" with our servers stamp aka the jwt_secret and then from there if it matches then we are good  */
