fcaff84e7a204a3846fbc963431f2058b8bfa44b
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-05-16 21:01:21 -0400

    working ont he services part

--- backend/services/jwt.service.ts
+++ backend/services/jwt.service.ts
@@ -1,2 +1,18 @@
 import jwt from 'jsonwebtoken'; 
 
+// import our secret keys here 
+const SECRET = process.env.JWT_SECRET as string; 
+const EXPIRATION = process.env.JWT_EXPIRES_IN as string; 
+
+// set the default payload here 
+export interface jwtPayload { 
+    Name: string, 
+    role: string, 
+    Email: string, 
+}; 
+
+// a function to actually create a token from the payload 
+export function signToken(payload: jwtPayload) : string { 
+    const token = jwt.sign(payload, SECRET, { expiresIn: EXPIRATION});  
+    return token; 
+}
